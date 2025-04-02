14160d578a82df304c2fc7a7aa1a4ade4cb5e11b
nurdin2004 <adiaderkisohouf@gmail.com>
2025-04-02 13:19:48 +0300

    submiting assignment

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
-const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+// code your solution here
+const record = [
+  { year: "2015", result: "Wl" },
+  { year: "2014", result: "N/A" },
+  { year: "2013", result: "L" },
 ];
-const titleCased =() =>{
-return tutorials.map(function (sentence) {
-  return sentence
-    .split(" ").map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-    .join(" ");
-});
-};
 
-console.log(titleCased());
+function superbowlWin(record){
+  const win = record.find(game => game.result === "W");
+  if (win) {
+        return win.year;
+    } else {
+        return undefined;
+    }
+}
+console.log(superbowlWin(record));
